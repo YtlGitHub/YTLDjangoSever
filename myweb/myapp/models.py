282281e3f0ba2800b0ef755b32487c9c1838bcc0
杨天龙 <1766842398@qq.com>
2021-09-13 20:40:55 +0800

    修改错误

--- myweb/myapp/models.py
+++ myweb/myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Stu(models.Model):
-    ```自定义Stu表对应的Model类```
+    '''自定义Stu表对应的Model类'''
     # 定义属性：默认主键自增id字段可不写
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=16)
