4382566f91d94b18f8fb9443fb483e84b4511443
杨天龙 <1766842398@qq.com>
2021-10-15 18:19:25 +0800

    增加上传图片代码

--- mydemo/myapp/urls.py
+++ mydemo/myapp/urls.py
@@ -3,12 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('users/', views.indexUsers, name='indexUsers'),
-    path('users/add/', views.addUsers, name="addUsers"),
-    path('users/insert/', views.insertUsers, name="insertUsers"),
-    path('users/del/<int:uid>/', views.delUsers, name="delUsers"),
-    path('users/edit/<int:uid>/', views.editUsers, name="editUsers"),
-    path('users/update/', views.updateUsers, name="updateUsers"),
-    path('inherit/', views.inherit, name="inherit"),
+    path('', views.index, name="index"),  # 首页
+    path('users/', views.index_users, name='indexUsers'),  # 浏览用户信息
+    path('users/add/', views.add_users, name="addUsers"),  # 加载添加用户信息
+    path('users/insert/', views.insert_users, name="insertUsers"),  # 执行用户信息添加
+    path('users/del/<int:uid>/', views.del_users, name="delUsers"),  # 执行用户信息删除
+    path('users/edit/<int:uid>/', views.edit_users, name="editUsers"),  # 加载用户信息修改表单
+    path('users/update/', views.update_users, name="updateUsers"),  # 执行用户信息修改
+    path('inherit/', views.inherit, name="inherit"),  # 模板继承
+    path('upload/', views.upload, name="upload"),  # 加载文件上传表单
+    path('doUpload/', views.do_upload, name="doUpload"),  # 执行文件上传处理
 ]
--- mydemo/myapp/views.py
+++ mydemo/myapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, Http404
 from django.urls import reverse  # 导入反向解析地址
 from myapp.models import Users
 from datetime import datetime
+from PIL import Image
+import time
+import os
 # Create your views here.
 
 
@@ -17,11 +20,11 @@ def index(request):
     # return HttpResponse("首页 <br> <a href='/users'>用户信息管理</a>")
     context = {}
     context['time'] = datetime.now
-    return render(request, "myapp/users/index01.html", context)
+    return render(request, "myapp/index/index.html", context)
 
 
 # 浏览用户信息
-def indexUsers(request):
+def index_users(request):
     #try:
         ulist = Users.objects.all()
         context = {"userslist": ulist}
@@ -31,12 +34,12 @@ def indexUsers(request):
 
 
 # 加载添加用户信息
-def addUsers(request):
+def add_users(request):
     return render(request, "myapp/users/add.html")
 
 
 # 执行用户信息添加
-def insertUsers(request):
+def insert_users(request):
     try:
         ob = Users()
         # 从表单中获取要添加的信息并封装到ob对象里面
@@ -52,7 +55,7 @@ def insertUsers(request):
 
 
 # 执行用户信息删除
-def delUsers(request, uid=0):
+def del_users(request, uid=0):
     try:
         ob = Users.objects.get(id=uid)  # 获取要删除的数据
         ob.delete()  # 执行删除操作
@@ -63,7 +66,7 @@ def delUsers(request, uid=0):
 
 
 # 加载用户信息修改表单
-def editUsers(request, uid=0):
+def edit_users(request, uid=0):
     try:
         ob = Users.objects.get(id=uid)  # 获取要修改的数据
         context = {"user": ob}
@@ -74,7 +77,7 @@ def editUsers(request, uid=0):
 
 
 # 执行用户信息修改
-def updateUsers(request):
+def update_users(request):
     try:
         uid = request.POST['id']  # 获取要修改书记的id号
         ob = Users.objects.get(id=uid)  # 查询要修改的数据
@@ -90,6 +93,33 @@ def updateUsers(request):
         context = {"info": "修改失败"}
     return render(request, "myapp/users/info.html", context)
 
+
 # 模板继承
 def inherit(request):
     return render(request, "myapp/inherit/sub_template.html")
+
+
+# 加载文件上传表单
+def upload(request):
+    return render(request, "myapp/upload/upload.html")
+
+
+# 执行文件上传处理
+def do_upload(request):
+    myFile = request.FILES.get("pic", None)
+    print(myFile)
+    print(request.POST.get("title"))
+    if not myFile:
+        return HttpResponse("没有上传的文件信息")
+    fileName = str(time.time())+'.'+myFile.name.split('.').pop()
+    destination = open(f"./static/pics/{fileName}", "wb+")  # w读取b二进制，图片音频都属于二进制
+    for chunk in myFile.chunks():  # 分块读取上传文件内容并写入目标文件
+        destination.write(chunk)
+    destination.close()  # 打开写入之后我们要关闭，用.close()这个方法关闭写入的文件
+    # # 执行图片缩放
+    # im = Image.open("./static/pics/"+fileName)
+    # # 缩放到75*75(缩放后的宽高比例不变)
+    # im.thumbnail((75, 75))
+    # # 把缩放后的图像用jpeg格式保存
+    # im.save("./static/pics/s_"+fileName, None)
+    return HttpResponse("上传的文件："+fileName)
